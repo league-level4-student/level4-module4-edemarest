b809817da63bfc5dda3b09ffcd54aae0cf174e30
edemarest <Lunacat7!>
2020-04-20 15:28:58 -0700

    4/20/20

--- src/_03_polymorphs/Polymorph.java
+++ src/_03_polymorphs/Polymorph.java
@@ -1,14 +1,21 @@
 package _03_polymorphs;
 
 import java.awt.Graphics;
-
+//* 1. Make the member variable private (or protected)
+//* 
+//* 2. Make a getter and setter method (accessor and mutator) for the variable.
+//* 
+//* 3. Add restrictions to the setter method so the member variable cannot be 
+//* 	  adversely altered
 public abstract class Polymorph {
-    int x;
-    int y;
+    private int x=;
+    private int y;
+    protected int width;
+    protected int height;
     
     Polymorph(int x, int y){
-   	 this.x = x;
-   	 this.y = y;
+   	 this.setX(x);
+   	 this.setY(y);
     }
     
     public void update(){
@@ -16,4 +23,36 @@ public abstract class Polymorph {
     }
     
     public abstract void draw(Graphics g);
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
 }
